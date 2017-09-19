cc9113c83f3dde4af6935d85f26c50c698f65fc3
HONG <mh106p@att.com>
2017-09-19 13:44:46 -0700

    first commit to push local packages to repository

---
+++ app/scripts/controllers/mynewcontroller.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name codesiteyoApp.controller:MynewcontrollerCtrl
+ * @description
+ * # MynewcontrollerCtrl
+ * Controller of the codesiteyoApp
+ */
+angular.module('codesiteyoApp')
+  .controller('MynewcontrollerCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
